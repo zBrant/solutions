a1c3d76872cafa4e6d88c7bd0dc013940ec6ff09
EduardoBrant <carlos23brant@hotmail.com>
2023-08-09 22:49:37 -0400

    update

--- hackerRank/c/variadicFunctions.c
+++ hackerRank/c/variadicFunctions.c
@@ -1,47 +1,47 @@
 int  sum (int count,...) {
-    int som = 0;
+    int s = 0;
     
     va_list ptr;
     
     va_start(ptr, count);
     
     for(int i = 0; i<count;++i)
-        som += va_arg(ptr, int);
+        s += va_arg(ptr, int);
         
     va_end(ptr);
     
-    return som;
+    return s;
 }
 
 int min(int count,...) {
-    int som = MIN_ELEMENT;
+    int m = MIN_ELEMENT;
     
     va_list ptr;
     
     va_start(ptr, count);
     
     for(int i = 0; i<count;++i){
-        if(som > va_arg(ptr, int))
-            som = va_arg(ptr, int);
+        if(m > va_arg(ptr, int))
+            m = va_arg(ptr, int);
     }
     va_end(ptr);
     
-    return som;
+    return m;
 }
 
 int max(int count,...) {
-     int som = MAX_ELEMENT;
+     int m = MAX_ELEMENT;
     
     va_list ptr;
     
     va_start(ptr, count);
     
     for(int i = 0; i<count;++i){
-        if(som < va_arg(ptr, int))
-            som = va_arg(ptr, int);
+        if(m < va_arg(ptr, int))
+            m = va_arg(ptr, int);
     }
     va_end(ptr);
     
-    return som;
+    return m;
 }
 
